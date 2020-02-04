239dff7d831da9d4b80dafd4b6bc264f4236b01f
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2020-02-04 20:30:35 -0300

    First commit.

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject codeminer-test "0.1.0-SNAPSHOT"
+(defproject codeminer_test "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot codeminer-test.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler codeminer-test.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
