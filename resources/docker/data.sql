b2a05af26db029928c6b2bba84d2cd9ec106d0d5
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2020-02-06 07:08:54 -0300

    [#5] Criado dados necessários na inicialização do postgres de forma a ser realizado testes na aplicação.

--- resources/docker/data.sql
+++ resources/docker/data.sql
@@ -1,5 +1,19 @@
 \c codeminer_test;
 
-insert into sometable(id, name) values (1, 'Teste');
+INSERT INTO ITEM_SPECIFICATION (name, points) VALUES ('Water', 4);
+INSERT INTO ITEM_SPECIFICATION (name, points) VALUES ('Food', 3);
+INSERT INTO ITEM_SPECIFICATION (name, points) VALUES ('Medication', 2);
+INSERT INTO ITEM_SPECIFICATION (name, points) VALUES ('Ammunition', 1);
 
-commit;
+INSERT INTO SURVIVOR (name, age, gender, latitude, longitude) VALUES ('Peter A. Ramaldes', 24, 'M', 50, 100);
+INSERT INTO SURVIVOR (name, age, gender, latitude, longitude) VALUES ('Pamela A. Fracalossi', 25, 'F', 250, 300);
+
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Peter A. Ramaldes'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Water'), 0);
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Peter A. Ramaldes'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Food'), 0);
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Peter A. Ramaldes'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Medication'), 0);
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Peter A. Ramaldes'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Ammunition'), 0);
+
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Pamela A. Fracalossi'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Water'), 0);
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Pamela A. Fracalossi'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Food'), 0);
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Pamela A. Fracalossi'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Medication'), 0);
+INSERT INTO INVENTORY (id_survivor, id_item, amount) VALUES ((SELECT ID FROM SURVIVOR WHERE name = 'Pamela A. Fracalossi'), (SELECT ID FROM ITEM_SPECIFICATION WHERE name = 'Ammunition'), 0);
--- resources/docker/schema.sql
+++ resources/docker/schema.sql
@@ -4,4 +4,24 @@ GRANT ALL PRIVILEGES ON DATABASE "codeminer_test" to codeminer_user;
 
 \c codeminer_test;
 
-create table sometable(id int, name char(10));
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE SURVIVOR (id uuid DEFAULT uuid_generate_v4(),
+                       name VARCHAR NOT NULL,
+                       age INTEGER NOT NULL,
+                       gender CHAR(1) NOT NULL,
+                       latitude INTEGER NOT NULL,
+                       longitude INTEGER NOT NULL,
+                       PRIMARY KEY (id));
+
+CREATE TABLE ITEM_SPECIFICATION (id uuid DEFAULT uuid_generate_v4(),
+                                 name VARCHAR NOT NULL,
+                                 points INTEGER NOT NULL,
+                                 PRIMARY KEY (id));
+
+CREATE TABLE INVENTORY (id_survivor uuid NOT NULL,
+                        id_item uuid NOT NULL,
+                        amount INTEGER NOT NULL,
+                        PRIMARY KEY (id_survivor, id_item),
+                        FOREIGN KEY (id_survivor) REFERENCES SURVIVOR (id),
+                        FOREIGN KEY (id_item)     REFERENCES ITEM_SPECIFICATION (id));
