6eae3b1931589010960295a98443f3b399bf6f99
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2020-02-07 22:41:34 -0300

    [#6] Criado arquivo com queries a serem utilizadas no desenvolvimento, para testes.

--- resources/queries/queries.clj
+++ resources/queries/queries.clj
@@ -1,6 +1,11 @@
 (ns queries.queries
- (:require [codeminer-test.connection-string :refer [db]]
-           [codeminer-test.db.sql :as sql]
-           [clojure.pprint :as pprint]))
+ (:require [codeminer-test.db.connection-string :refer [db]]
+           [codeminer-test.db.sql :as sql]))
 
-(sql/survivors db {:cols ["name"]})
+(sql/survivors-all db)
+
+(sql/survivors db {:cols ["name" "age"]})
+
+(sql/survivor-by-id db {:id #uuid"9e01df0b-d816-4c83-8fcd-86f50664db1b"})
+
+(sql/survivors-by-name-like db {:name-like "P%"})
--- src/codeminer_test/db/sql/survivor.sql
+++ src/codeminer_test/db/sql/survivor.sql
@@ -1,16 +1,14 @@
--- :name survivors-all
--- :command :query
--- :result n
+-- :name survivors-all :? :*
 -- :doc select all the survivors with all the attributes
 SELECT *
-  FROM survivors
+    FROM survivor
 
 -- :name survivors
 -- :command :query
 -- :result n
 -- :doc select all with cols defined by {:cols [<col_name>...]}
 SELECT :i*:cols
-  FROM survivors
+  FROM survivor
 
 -- Note the terse style below
 -- ":command :query" -> ":?"
@@ -19,12 +17,12 @@ SELECT :i*:cols
 -- :name survivor-by-id :? :*
 -- :doc get survivor info by id
 SELECT *
-  FROM survivors
+  FROM survivor
  WHERE id = :id
 
 -- :name survivors-by-name-like :? :*
 -- :doc use {:name-like "P%"} as the option param to get the P's
 SELECT *
-  FROM survivors
+  FROM survivor
  WHERE name
   LIKE :name-like
